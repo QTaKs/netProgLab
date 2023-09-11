6d75595c1f7cdb6b91be58f2b06c78e524b7a19b
qtaks <ror.25@mail.ru>
2023-09-11 19:43:42 +0700

    Текущая ветка: main  Эта ветка соответствует «origin/main».
     Изменения, которые будут включены в коммит:
    	новый файл:    webapp/.gitignore
    	новый файл:    webapp/.mvn/wrapper/maven-wrapper.jar
    	новый файл:    webapp/.mvn/wrapper/maven-wrapper.properties
    	новый файл:    webapp/mvnw
    	новый файл:    webapp/mvnw.cmd
    	новый файл:    webapp/pom.xml
    	новый файл:    webapp/src/main/java/org/qtstu/webapp/WebappApplication.java
    	новый файл:    webapp/src/main/resources/application.properties
    	новый файл:    webapp/src/test/java/org/qtstu/webapp/WebappApplicationTests.java

---
+++ webapp/src/test/java/org/qtstu/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.qtstu.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
