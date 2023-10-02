9c13ea8a52c0237255b4f5103f01b21b74035257
qtaks <ror.25@mail.ru>
2023-10-02 17:01:16 +0700

    Compiling and working base

---
+++ webapp/src/main/java/org/qtstu/webapp/Controllers/MainController.java
@@ -0,0 +1,18 @@
+package org.qtstu.webapp.Controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class MainController {
+
+
+    //    @GetMapping
+//    public BaseResponse showStatus() {
+//        return new BaseResponse(SUCCESS_STATUS, 1);
+//    }
+
+}
---
+++ webapp/src/main/java/org/qtstu/webapp/Controllers/RootController.java
@@ -0,0 +1,15 @@
+package org.qtstu.webapp.Controllers;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String index() {
+        return  """
+                <p>Where is nothing to see. See:</p>
+                """;
+    }
+
+}
